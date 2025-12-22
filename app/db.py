6f1517840ab64ad0af65edde2cd92161c0ac134c
May Lee <mayhclee@gmail.com>
2025-12-22 10:08:16 -0800

    updated db to include Base

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from .models.base import Base
 
-db = SQLAlchemy()
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
